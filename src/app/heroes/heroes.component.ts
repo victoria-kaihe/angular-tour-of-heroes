10a1ff444203ac8eed948d0b6b9ae1d62f494ea4
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-03-01 10:53:33 +1300

    add click event handler

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,10 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
   constructor() { }
 
   ngOnInit() {
