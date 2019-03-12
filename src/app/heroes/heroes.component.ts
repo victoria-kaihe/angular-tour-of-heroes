bba5b4071fdf5fe1e096ea9c26f0bcea62474f81
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-03-13 11:52:31 +1300

    add getHeroes func to retrieve heroes from service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,4 +20,8 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.getHeroes.heroes = this.heroService.getHeroes()
+  }
 }
